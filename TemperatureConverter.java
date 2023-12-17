c98829ca1e6f0a481372465403113a0e5a773691
LaxmiBH <lalubhutad@gmail.com>
2023-12-17 11:34:04 +0530

    Chnages Commit

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -4,15 +4,15 @@ public class TemperatureConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
- //Accept Temperature value       
+    
         System.out.print("Enter temperature value: ");
         double temperature = scanner.nextDouble();
 
- //Accept Temperature Unit
+
         System.out.print("Enter temperature unit (C or F): ");
         char unit = scanner.next().charAt(0);
 
- //COnert and Display Temeprature in  the other unit      
+ 
         if (unit == 'C' || unit == 'c') {
             double convertedTemperature = celsiusToFahrenheit(temperature);
             System.out.println(temperature + " Celsius is equal to " + convertedTemperature + " Fahrenheit.");
@@ -27,12 +27,12 @@ else {
         scanner.close();
     }
 
-//Method to Convert Celcius to Fahrenheit    
+
     private static double celsiusToFahrenheit(double celsius) {
         return (celsius * 9/5) + 32;
     }
 
-   //Method to Convert to Fahrenheit to Celsius
+
     private static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5/9;
     }
